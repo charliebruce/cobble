5f2d2c16c69a7b6205f47f6f9c5a456082c3aaea
Charlie <charliebruce@gmail.com>
2022-03-18 17:15:22 +0000

    Generate library as well as scan example binary

--- src/make_linux.sh
+++ src/make_linux.sh
@@ -7,3 +7,10 @@ cobble_scan_example.c \
 platforms/linux/linux_bluez.cpp \
 -std=c++0x $(pkg-config dbus-1 --cflags) -ldbus-1 -lpthread -Werror -Wall -Wextra \
 -o build/cobble_linux
+
+# Library (default architecture)
+g++ -shared \
+cobble_events.c \
+platforms/linux/linux_bluez.cpp \
+-std=c++0x $(pkg-config dbus-1 --cflags) -ldbus-1 -lpthread -Werror -Wall -Wextra \
+-o build/libcobble.so
